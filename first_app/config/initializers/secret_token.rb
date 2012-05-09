de5258c09e7005c733d3361e07436493407b81d0
Ryan Andersen <ryan.andersen91@gmail.com>
2012-05-09 11:44:34 -0600

    Branch merge

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '69b7da41ea03eb8965cd1fa7f142a3ff59d8e83dd2f9e54af0f5afb3981c49d6c482927f9625bdbd624b597d70550451484f352928fc479f98d5b5c21d2169a1'
