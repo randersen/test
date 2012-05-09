e0b4e5c49a002ffa4aed7b864ac7886af498427a
Ryan Andersen <ryan.andersen91@gmail.com>
2012-05-09 09:51:49 -0600

    Added site_settings.rb

--- blog/config/environment.rb
+++ blog/config/environment.rb
@@ -1,3 +1,6 @@
+# Load site-wide Constants / Settings
+require File.join( Rails.root.to_s, 'config', 'site_settings') #or use Dir.pwd
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
---
+++ blog/config/site_settings.rb
@@ -0,0 +1,6 @@
+# Site-Wide Settings File ./config/site_settings.rb
+# This file defines Modules and Constants which should be used consistently throughout the application
+module Site
+  Name    = NameShort = "Ryan's Blog"
+  NameCom = NameLong  = "Ryansblog.com"
+end
